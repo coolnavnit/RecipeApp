5577b2c5505763ec41aa53d847ea0cd6ef0c1d01
navnit.punj <navnit.punj@cerillion.com>
2019-12-31 15:54:38 +0000

    More changes

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/Ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('salt',1,'tbsp'),
+    new Ingredient('chilli',2,'pc')
+  ];
   constructor() { }
 
   ngOnInit() {
